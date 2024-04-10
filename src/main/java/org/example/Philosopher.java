32b113c6a0d68e99deb1618255dadc2b3e085656
admin <jackybs@mail.ru>
2024-04-10 17:32:56 +0300

    Initial commit

---
+++ src/main/java/org/example/Philosopher.java
@@ -0,0 +1,61 @@
+package org.example;
+
+
+    import java.util.concurrent.CountDownLatch;
+
+    public class Philosopher implements Runnable {
+        static private final int TIME_TO_EAT = 500;
+
+        private final String name;
+        private final int timeToThink;
+        private final CountDownLatch latch;
+
+        public Philosopher(String name, int timeToThink, CountDownLatch latch) {
+            this.name = name;
+            this.latch = latch;
+            this.timeToThink = timeToThink;
+        }
+
+        void think() {
+            System.out.println(Thread.currentThread().getName() + " is thinking!");
+            try {
+                Thread.sleep(timeToThink);
+            } catch (InterruptedException e) {
+                System.out.println(Thread.currentThread().getName() + " is interrupted!");
+            }
+        }
+
+        static synchronized void haveLunch() {
+            try {
+                Thread.sleep(TIME_TO_EAT);
+            } catch (InterruptedException e) {
+                System.out.println("Is interrupted!");
+            }
+        }
+
+        @Override
+        public void run() {
+            for (int i = 1; i < 4; i++) {
+                haveLunch();
+                switch (i) {
+                    case 1 -> Main.latch1.countDown();
+                    case 2 -> Main.latch2.countDown();
+                    case 3 -> Main.latch3.countDown();
+                }
+                System.out.println(name + " had lunch " + i + " time(s)");
+                think();
+
+                try {
+                    switch (i) {
+                        case 1 -> Main.latch1.await();
+                        case 2 -> Main.latch2.await();
+                        case 3 -> Main.latch3.await();
+                    }
+                } catch (InterruptedException e) {
+                    System.out.println("Interrupted " + Thread.currentThread().getName());
+                }
+            }
+            latch.countDown();
+        }
+    }
+
